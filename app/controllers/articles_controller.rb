bf49abd5b9fc51f335b377b627cd6fec0a108c4a
Rajan Singh <rajansingh96@gmail.com>
2019-12-29 00:09:56 +0530

    Update starting now properly

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,24 @@
 class ArticlesController < ActionController::Base
 
+	def new
+		@article = Article.new
+	end
 
+	def create 
+		@article = Article.new(article_params)
+		if @article.save
+			
+		else
+
+		end
+	end
+
+	private
+
+	def article_params
+		params.require(:article).permit(:title,:description)
+	end
 
 
 end
+
